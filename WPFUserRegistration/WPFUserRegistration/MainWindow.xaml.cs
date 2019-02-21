0f2e52dbc1bc4f436dd9636b02e37f7bdcef62ff
Matthew Shaw <mshaw@sparta.com>
2019-02-21 19:48:52 +0000

    Modified database entry system

---
+++ WPFUserRegistration/WPFUserRegistration/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+
+namespace WPFUserRegistration
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        User kc;
+        public MainWindow()
+        {
+            InitializeComponent();
+            kc = new User();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string item = nameField.Text;
+            WritetoFile(item);
+
+        }
+
+        public void WritetoFile(string item)
+        {
+            string path = "user.txt";
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(item);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Choose a photo";
+            ofd.Filter = "All Supported image | *.jpeg;*.jpg;*.png";
+            if (ofd.ShowDialog() == true)
+            {
+                imageField.Source = new BitmapImage(new Uri(ofd.FileName));
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void registerUser(object sender, RoutedEventArgs e)
+        {
+            kc.Name = nameField.Text;
+            kc.Password = passwordField.Password;
+            kc.Age = DateTime.Now.Year - ageField.SelectedDate.Value.Year;
+            kc.Sex = sexBox.Text;
+            kc.PicturePath = imageField.Source.ToString();
+            kc.PhoneNumber = numberField.Text;
+            if (checkbox_0.IsChecked == true)
+            {
+                kc.Transportation[0] = "Walks";
+            }
+            if (checkbox_1.IsChecked == true)
+            {
+                kc.Transportation[1] = "Drives";
+            }
+            if (checkbox_2.IsChecked == true)
+            {
+                kc.Transportation[2] = "Cycles";
+            }
+
+            if (radio_0.IsChecked == true)
+            {
+                kc.AgeRange = "18-";
+            }
+            if (radio_1.IsChecked == true)
+            {
+                kc.AgeRange = "18+";
+            }
+            kc.saveData();
+        }
+
+        private void ViewProfile(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult mbr = MessageBox.Show("Enter a Username");
+            string userName = mbr.ToString();
+            MessageBox.Show(userName);
+            //User mc = new User();
+            string[] sr = File.ReadAllLines("SchholDB.csv");
+            string[] str = sr[0].Split(',');
+            nameField.Text = str[0]; 
+
+        }
+    }
+}
---
+++ WPFUserRegistration/WPFUserRegistration/User.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WPFUserRegistration
+{
+    public class User
+    {
+        string name;
+        string password;
+        int age;
+        string sex;
+        string phoneNumber;
+        string picturePath;
+        string ageRange;
+        string[] transportation = new string [3];
+
+        public string Name { get => name; set => name = value; }
+        public string Password { get => password; set => password = value; }
+        public int Age { get => age; set => age = value; }
+        public string Sex { get => sex; set => sex = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string PicturePath { get => picturePath; set => picturePath = value; }
+        public string AgeRange { get => ageRange; set => ageRange = value; }
+        public string[] Transportation { get => transportation; set => transportation = value; }
+        public User()
+        {
+
+
+        }
+        public User(string n, string p, int a, string s, string pn, string pix, string ar, string[] t)
+        {
+            name = n;
+            password = p;
+            age = a;
+            sex = s;
+            phoneNumber = pn;
+            picturePath = pix;
+            ageRange = ar;
+            transportation = t;
+        }
+
+        public void saveData()
+        {
+            string eachItem = "";
+            foreach (var item in transportation)
+            {
+                eachItem += "_" + item;
+
+            }
+
+            using (StreamWriter sw = File.AppendText("UserData.csv"))
+            {
+                sw.WriteLine(name + ":" + password + ":" + age + ":" + sex +":" + phoneNumber + ":" + picturePath + ":" + ageRange + ":" + transportation);
+                
+
+
+            }
+        }
+    }
+}
